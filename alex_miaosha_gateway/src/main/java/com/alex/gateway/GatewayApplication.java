f970c0eff0dc4c66fe95c1960afb4ca6ccb926af
majf <734663446@qq.com>
2022-09-29 17:51:00 +0800

    添加gateway RateLimiter

--- alex_miaosha_gateway/src/main/java/com/alex/gateway/GatewayApplication.java
+++ alex_miaosha_gateway/src/main/java/com/alex/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.alex.gateway;
 import com.alex.common.config.WebMvcConfigurer;
 import com.alex.common.config.qiniu.QiNiuConfiguration;
 import com.alex.common.utils.qiniu.ImageScalaKit;
+import com.alex.utils.interceptor.SeckillInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,17 +12,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 /**
- * @description:  gateway启动类
- * @author:       majf
- * @createDate:   2022/7/29 14:18
- * @version:      1.0.0
+ * @description: gateway启动类
+ * @author: majf
+ * @createDate: 2022/7/29 14:18
+ * @version: 1.0.0
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.alex.gateway", "com.alex.common", "com.alex.utils"},
         excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                value = {QiNiuConfiguration.class, ImageScalaKit.class, WebMvcConfigurer.class}
-)})
+                value = {QiNiuConfiguration.class, ImageScalaKit.class, WebMvcConfigurer.class, SeckillInterceptor.class}
+        )})
 public class GatewayApplication {
 
     public static void main(String[] args) {
