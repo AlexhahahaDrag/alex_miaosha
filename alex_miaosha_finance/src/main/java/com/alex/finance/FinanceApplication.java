93d3fcdc30fe63cf7369ae31ae8c7fb992305bec
alexhahahaDrag <734663446@qq.com>
2022-10-10 07:21:16 +0800

    添加财务模块

---
+++ alex_miaosha_finance/src/main/java/com/alex/finance/FinanceApplication.java
@@ -0,0 +1,18 @@
+package com.alex.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: alex
+ * @createDate: 2022/10/10 6:51
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class FinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceApplication.class, args);
+    }
+}
---
+++ doc/alex_finance.sql
@@ -0,0 +1,46 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80020
+ Source Host           : localhost:3306
+ Source Schema         : alex_finance
+
+ Target Server Type    : MySQL
+ Target Server Version : 80020
+ File Encoding         : 65001
+
+ Date: 10/10/2022 07:15:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for finance_info
+-- ----------------------------
+DROP TABLE IF EXISTS `finance_info`;
+CREATE TABLE `finance_info`  (
+  `id` bigint NOT NULL COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名称',
+  `type_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '类别',
+  `amount` decimal(10, 0) NULL DEFAULT NULL COMMENT '钱数',
+  `from_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '来源',
+  `is_valid` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否有效',
+  `is_delete` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '是否删除',
+  `creator` bigint COMMENT '创建人',
+  `updater` bigint COMMENT '更新人',
+  `deleter` bigint COMMENT '删除人',
+  `created_time` datetime COMMENT '创建时间',
+  `updated_time` datetime COMMENT '更新时间',
+  `delete_time` datetime COMMENT '更新时间',
+  `operate_time` datetime COMMENT '操作时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of finance_info
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
