506b839c5dd963d4b04aae6efcc438b34d5065b7
majf <734663446@qq.com>
2023-04-28 14:05:30 +0800

    测试ip获取

--- alex_miaosha_finance/finance_boot/src/main/java/com/alex/finance/service/weixin/impl/WeiXinServiceImpl.java
+++ alex_miaosha_finance/finance_boot/src/main/java/com/alex/finance/service/weixin/impl/WeiXinServiceImpl.java
@@ -4,8 +4,12 @@ import com.alex.finance.config.WechatAccountConfig;
 import com.alex.finance.service.weixin.WeiXinService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.menu.WxMpGetSelfMenuInfoResult;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
+import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.apache.commons.compress.utils.Lists;
@@ -27,28 +31,26 @@ public class WeiXinServiceImpl implements WeiXinService {
     }
 
     public String sentMessage(String account, Long num) throws WxErrorException {
-        String result = getToken();
+        String token = getToken();
         List<WxMpTemplateData> dataList = Lists.newArrayList();
         dataList.add(new WxMpTemplateData("account", account, "#A9A9A9"));
         dataList.add(new WxMpTemplateData("num", num.toString(), "#A9A9A9"));
         dataList.add(new WxMpTemplateData("type", "猫超卡", "#A9A9A9"));
         WxMpTemplateMessage templateMessage = WxMpTemplateMessage.builder()
                 .templateId(wechatAccountConfig.getAccountTimeOutTemplateId())
-                .toUser(wechatAccountConfig.getUserId())
-                .url(wechatAccountConfig.getUrl() + "/wxaapi/newtmpl/getpubtemplatetitles?access_token=" + result)
+//                .toUser(wechatAccountConfig.getUserId())
+                .url(wechatAccountConfig.getUrl() + "/wxaapi/newtmpl/getpubtemplatetitles?access_token=" + token)
                 .data(dataList)
                 .build();
-        // TODO: 2023/4/12 添加查询用户列表 
-        String msgId = wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage);
-        WxMpTemplateMessage templateMessage1 = WxMpTemplateMessage.builder()
-                .templateId(wechatAccountConfig.getAccountTimeOutTemplateId())
-                .toUser("")
-                .url(wechatAccountConfig.getUrl() + "/wxaapi/newtmpl/getpubtemplatetitles?access_token=" + result)
-                .data(dataList)
-                .build();
-        String msgId1 = wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage1);
-        log.info("微信网页授权，获取code，result={}", result);
+        // 查询用户列表
+        WxMpUserList wxMpUserList = wxMpService.getUserService().userList(null);
+        if (wxMpUserList.getTotal() > 0) {
+            for (String userId : wxMpUserList.getOpenids()) {
+                templateMessage.setToUser(userId);
+                wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage);
+            }
+        }
         wxMpService.getCurrentAutoReplyInfo().getKeywordAutoReplyInfo();
-        return msgId1;
+        return "success";
     }
 }
