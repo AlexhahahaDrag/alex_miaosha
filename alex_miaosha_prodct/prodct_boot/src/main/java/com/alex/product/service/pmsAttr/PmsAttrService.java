315e42826d6bd9974e4df2e13c53c56112939530
alexhaha <734663446@qq.com>
2024-01-09 23:08:55 +0800

    修改明细路径名称-未测试

---
+++ alex_miaosha_prodct/prodct_boot/src/main/java/com/alex/product/service/pmsAttr/PmsAttrService.java
@@ -0,0 +1,26 @@
+package com.alex.product.service.pmsAttr;
+
+import com.alex.api.product.vo.pmsAttr.PmsAttrVo;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.alex.product.entity.pmsAttr.PmsAttr;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 商品属性 服务类
+ * @author: alex
+ * @createDate: 2023-12-21 22:50:20
+ * @description: 我是由代码生成器生成
+ * version: 1.0.0
+ */
+public interface PmsAttrService extends IService<PmsAttr> {
+
+    Page<PmsAttrVo> getPage(Long pageNum, Long pageSize, PmsAttrVo pmsAttrVo);
+
+    PmsAttrVo queryPmsAttr(String id);
+
+    Boolean addPmsAttr(PmsAttrVo pmsAttrVo);
+
+    Boolean updatePmsAttr(PmsAttrVo pmsAttrVo);
+
+    Boolean deletePmsAttr(String ids);
+}
